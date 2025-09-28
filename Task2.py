9d324bda1845267d15e5e68133697f81bb6d1c63
Aryan Mohite <aryanmohite6625@gmail.com>
2025-09-28 21:19:01 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,4 +1,4 @@
-a=(str(input("Enter your Frist name: ")))
+a=(str(input("Enter your First name: ")))
 b=(str(input("Enter your Last name: ")))
 c=(str(a) +" "+ str(b))
 print("\nHello, " +c +"! Welcome to the Python program.")
