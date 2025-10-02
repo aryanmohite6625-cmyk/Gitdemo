f07fb00b4fec9d4145d2ad5e1cc9a4cedfa18632
Aryan6625 <aryanmohite6625@gmail.com>
2025-10-02 16:02:18 +0530

    Assignment3

---
+++ Task_2.py
@@ -0,0 +1,8 @@
+a = (float(input("Enter a number: ")))
+import math
+b = math.sqrt(a)
+print(f"square root is: ",b)
+c = math.log(a)
+print(f"logarithm is: ",c)
+d = math.sin(a)
+print(f"sine is: ",d)
