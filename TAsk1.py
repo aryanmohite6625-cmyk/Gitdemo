249e187d817b474758ad2277142fe75683fc3388
Aryan6625 <aryanmohite6625@gmail.com>
2025-10-25 15:16:44 +0530

    Assignment1

---
+++ TAsk1.py
@@ -0,0 +1,12 @@
+a=(float(input("Enter First Number: ")))
+b=(float(input("Enter Second Number: ")))
+
+
+c=a+b
+print("Addition:", c)
+d=a-b
+print("Subtraction:", d)
+e=a*b
+print("Multiplication:", e)
+f=a/b
+print("Division:", f)
